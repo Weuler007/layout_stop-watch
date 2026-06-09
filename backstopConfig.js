bedbd80bdfd74fd6b87d5182bd67bbe2288ec5e5
Weuler de Oliveira Vieira <jhdlfdf@hotmail.com>
2026-06-08 22:14:52 -0300

    test

--- backstopConfig.js
+++ backstopConfig.js
@@ -18,10 +18,7 @@ const config = {
   fileNameTemplate: '{scenarioLabel}',
   onBeforeScript: 'puppet/onBefore.js',
   onReadyScript: 'puppet/onReady.js',
-  engineOptions: {
 
-    executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
-  },
   viewports: [
     {
       name: 'tablet_h',
